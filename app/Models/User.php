3e43381cbafdd267cb4967ede283b4aea74d3929
KiranaAdityaMoza <kiranamoza4@gmail.com>
2026-09-23 12:50:53 +0700

    Selesai Langkah Praktikum Modul 4

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class UserModel extends Model
+class User extends Model
 {
     use HasFactory;
 
@@ -15,5 +15,12 @@ class UserModel extends Model
     public function kelas()
     {
         return $this->belongsTo(Kelas::class, 'kelas_id');
+    }
+
+    public function getUser()
+    {
+        return $this->join('kelas', 'kelas.id', '=', 'user.kelas_id')
+                    ->select('user.*', 'kelas.nama_kelas as nama_kelas')
+                    ->get();
     }
 }
